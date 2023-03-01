b29975b8c0b4788d5f46568738e6053dd95f8ac8
leandroeguimaraes <leandro.e.guimaraes@gmail.com>
2023-03-01 15:41:41 -0300

    docs: added docs to the auth module and app module

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Bem-vindo à API - Prontuário Eletrônico', () => {
+      expect(appController.getHello()).toBe(
+        'Bem-vindo à API - Prontuário Eletrônico',
+      );
     });
   });
 });
--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LocalAuthGuard } from 'src/common/guards/local-auth.guard';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
   @UseGuards(LocalAuthGuard)
+  @ApiOperation({ summary: 'Realiza o login de um usuário' })
+  @ApiResponse({ status: 200, description: 'Usuário autenticado com sucesso' })
+  @ApiResponse({ status: 401, description: 'Não autorizado' })
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
