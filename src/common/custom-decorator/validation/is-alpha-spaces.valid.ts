c0cd7e509701e8cdd06be4c9276461a2c836cb02
leandroeguimaraes <leandro.e.guimaraes@gmail.com>
2023-03-02 15:20:38 -0300

    test: added unit tests to custom decorator isAlphaSpaces

--- src/common/custom-decorator/validation/is-alpha-spaces.valid.ts
+++ src/common/custom-decorator/validation/is-alpha-spaces.valid.ts
@@ -10,9 +10,14 @@ export function IsAlphaSpaces(validationOptions?: ValidationOptions) {
       options: validationOptions,
       validator: {
         validate(value: any) {
+          if (value[0] === ' ' || value[value.length - 1] === ' ')
+            return false;
           const regex = /^[a-zA-Z ]*$/;
           const match = regex.test(value);
           return match && value;
+        },
+        defaultMessage() {
+          return `${propertyName} must contain only alphabetical characters and spaces`;
         },
       },
     });
